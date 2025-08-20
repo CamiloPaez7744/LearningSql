b4fcc1fed71bb48e1c1c24cb7a54f179496675f5
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-08-19 21:25:30 -0500

    Add SQL view for comments per week with post count and total claps

---
+++ tarea/mediumDB/141-views.sql
@@ -0,0 +1,11 @@
+
+CREATE VIEW comments_per_week as
+SELECT date_trunc('week', p.created_at) as weeks,
+	COUNT(DISTINCT p.post_id) as number_of_post,
+	SUM(c.counter) as total_claps
+FROM posts p
+INNER JOIN claps c ON c.post_id = p.post_id
+GROUP BY weeks
+ORDER BY weeks DESC;
+
+SELECT * FROM comments_per_week;
