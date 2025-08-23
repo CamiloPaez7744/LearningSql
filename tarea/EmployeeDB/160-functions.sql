59a4469498529e9e1f4f7c37700c7a8cd08dbf48
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-08-22 21:40:09 -0500

    Add functions for calculating possible raises and retrieve employee details

--- tarea/EmployeeDB/160-functions.sql
+++ tarea/EmployeeDB/160-functions.sql
@@ -11,3 +11,78 @@ LANGUAGE plpgsql;
 
 
 SELECT first_name, greed_employee(first_name) FROM employees;
+
+
+SELECT * FROM employees;
+
+SELECT * FROM jobs;
+
+SELECT
+  e.employee_id,
+  e.first_name,
+  e.salary,
+  j.max_salary,
+  j.max_salary - e.salary as possible_rase
+FROM
+  employees e
+  INNER JOIN jobs j ON j.job_id = e.job_id;
+  
+  
+CREATE
+OR REPLACE FUNCTION max_raise (empl_id INTEGER) RETURNS NUMERIC(8, 2) AS $$
+
+DECLARE
+possible_raise NUMERIC(8,2);
+
+BEGIN
+
+SELECT
+  j.max_salary - e.salary INTO possible_raise
+FROM
+  employees e
+  INNER JOIN jobs j ON j.job_id = e.job_id
+  WHERE e.employee_id = empl_id;
+	RETURN possible_raise;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE
+OR REPLACE FUNCTION max_raise2 (empl_id INTEGER) RETURNS NUMERIC(8, 2) AS $$
+
+DECLARE
+employee_job_id int;
+current_salary NUMERIC(8,2);
+job_max_salary NUMERIC(8,2);
+possible_raise NUMERIC(8,2);
+
+BEGIN
+
+SELECT
+  job_id,
+  salary INTO employee_job_id,
+  current_salary
+FROM
+  employees
+WHERE
+  employee_id = empl_id;
+  
+SELECT
+max_salary INTO job_max_salary
+FROM
+jobs
+WHERE
+job_id = employee_job_id;
+
+possible_raise = job_max_salary - current_salary;
+
+RETURN possible_raise;
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT
+  e.employee_id,
+  e.first_name,
+  max_raise(e.employee_id)
+FROM
+employees e;
